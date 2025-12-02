548d23e367011b076ea317e23720601b4702cadf
FilippoT <f.tacchini@visionix.com>
2025-12-02 11:54:59 +0100

    fix: allinea compileOptions Java a JDK 17 per compatibilità Kotlin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,6 +47,11 @@ android {
         }
     }
 
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
     kotlinOptions {
         jvmTarget = "17"
         freeCompilerArgs = freeCompilerArgs + listOf("-Xjvm-default=all")
